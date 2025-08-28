f09bebe09201538f6d464d828c1b12cbbc456bd5
Juan Carlos Herrera <juancabaron19@gmail.com>
2025-08-27 19:48:26 -0500

    tercer commit

---
+++ app/src/main/java/com/example/miprimeraapp/InicioActivity.kt
@@ -0,0 +1,19 @@
+package com.example.miprimeraapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class InicioActivity : AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+
+        val button = findViewById<Button>(R.id.bu_comenzar_ini)
+        button.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
